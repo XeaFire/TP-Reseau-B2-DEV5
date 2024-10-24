939a0767060ebbbf3016812956c0b0c52937a1bd
DTristan <tristan.diard@ynov.com>
2024-10-24 14:02:20 +0200

    TP5

--- tp5_enc_client_2.py
+++ tp5_enc_client_2.py
@@ -30,8 +30,15 @@ while valid:
     else:
         print("Votre input n'est pas valide")
     
+# Encodage à la Tristan Diarhée là
 
+numb1 = numbers[0]
+numb2 = numbers[0]
+shifted_i = numb1 << 20
+numbs = shifted_i | numb2
+numbs_bytes = numbs.to_bytes(5, byteorder='big')
 
+print(utils.bytes_to_bits_binary(numbs_bytes))
 # On envoie
 s.send(msg.encode())
 
