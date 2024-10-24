38c74b25a7af2dc792c8d9d0f3b2b28c4f4ddc47
DTristan <tristan.diard@ynov.com>
2024-10-24 13:01:47 +0200

    temp

--- test.py
+++ test.py
@@ -1,59 +1,26 @@
-import socket
+# msg = input("Calcul à envoyer: ")
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-s.bind(('5.5.5.11', 13337))  
+# encoded_msg = bin(int(msg))
 
-s.listen(1)
-conn, addr = s.accept()
+# print(encoded_msg[2:len(encoded_msg)])
 
-while True:
+def bytes_to_bits_binary(byte_data):
+    bits_data = bin(int.from_bytes(byte_data, byteorder='big'))[2:]
+    bits_data = bits_data.zfill(len(byte_data) * 8)
+    return bits_data
 
-    try:
-        # On reçoit la string Hello du client
-        data = conn.recv(1024)
-        if not data: break
-        print(f"Données reçues du client : {data}")
 
-        conn.send("Hello".encode())
 
-        # On reçoit le calcul du client
-        header = conn.recv(4)
-        if not header:
-            break
+int_i = 1048575
+shifted_i = 1048575 << 20
+int_f = 1
+shifted_i = shifted_i | int_f
+super_int_one_byte = shifted_i.to_bytes(5, byteorder='big')
 
-        # On lit la valeur
-        msg_len = int.from_bytes(header[0:4], byteorder='big')
+print(bytes_to_bits_binary(super_int_one_byte))
+i_bin = bytes_to_bits_binary(super_int_one_byte[0:5])[0:20]
+f_bin = bytes_to_bits_binary(super_int_one_byte[0:5])[20:40]
+print(i_bin)
+print(f_bin)
+print(int(i_bin, 2))
 
-        print(f"Lecture des {msg_len} prochains octets")
-
-        # Une liste qui va contenir les données reçues
-        chunks = []
-
-        bytes_received = 0
-        while bytes_received < msg_len:
-            # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
-            chunk = conn.recv(min(msg_len - bytes_received,1024))
-            if not chunk:
-                raise RuntimeError('Invalid chunk received bro')
-
-            # on ajoute le morceau de 1024 ou moins à notre liste
-            chunks.append(chunk)
-
-            # on ajoute la quantité d'octets reçus au compteur
-            bytes_received += len(chunk)
-
-        # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
-        message_received = b"".join(chunks).decode('utf-8')
-        print(f"Received from client {message_received}")
-        
-        
-        # Evaluation et envoi du résultat
-        res  = eval(message_received)
-        conn.send(str(res).encode())
-         
-    except socket.error:
-        print("Error Occured.")
-        break
-
-conn.close()
---
+++ tp5_enc_client_2.py
@@ -0,0 +1,23 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('5.5.5.11', 13337))
+s.send('Hello'.encode())
+
+# On reçoit la string Hello
+data = s.recv(1024)
+
+# Récupération d'une string utilisateur
+msg = input("Calcul à envoyer: ")
+
+encoded_msg = msg.encode('utf-8')
+
+print(encoded_msg)
+
+# On envoie
+s.send(msg.encode())
+
+# Réception et affichage du résultat
+s_data = s.recv(1024)
+print(s_data.decode())
+s.close()
