daacf859987cda285ea3a466f676466d0cfbd493
DTristan <tristan.diard@ynov.com>
2024-10-24 10:34:06 +0200

    TP5

--- test.py
+++ test.py
@@ -26,8 +26,30 @@ while True:
         msg_len = int.from_bytes(header[0:4], byteorder='big')
 
         print(f"Lecture des {msg_len} prochains octets")
+
+        # Une liste qui va contenir les données reçues
+        chunks = []
+
+        bytes_received = 0
+        while bytes_received < msg_len:
+            # Si on reçoit + que la taille annoncée, on lit 1024 par 1024 octets
+            chunk = conn.recv(min(msg_len - bytes_received,1024))
+            if not chunk:
+                raise RuntimeError('Invalid chunk received bro')
+
+            # on ajoute le morceau de 1024 ou moins à notre liste
+            chunks.append(chunk)
+
+            # on ajoute la quantité d'octets reçus au compteur
+            bytes_received += len(chunk)
+
+        # ptit one-liner pas combliqué à comprendre pour assembler la liste en un seul message
+        message_received = b"".join(chunks).decode('utf-8')
+        print(f"Received from client {message_received}")
+        
+        
         # Evaluation et envoi du résultat
-        res  = eval(data.decode())
+        res  = eval(bytes(message_received).decode())
         conn.send(str(res).encode())
          
     except socket.error:
