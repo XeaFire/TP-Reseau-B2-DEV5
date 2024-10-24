93e8f3f5296927a41d78a286909532f51876b449
DTristan <tristan.diard@ynov.com>
2024-10-24 15:44:43 +0200

    TP5

--- tp5_enc_server_2.py
+++ tp5_enc_server_2.py
@@ -14,14 +14,28 @@ while True:
         # On reçoit la string Hello du client
         data = conn.recv(1024)
         if not data: break
-        print(f"Données reçues du client : {data}")
-
         conn.send("Hello".encode())
 
         # On reçoit le calcul du client
         data = conn.recv(6)
+        data_bin = utils.bytes_to_bits_binary(data)
 
-        print(utils.bytes_to_bits_binary(data))
+        operateur_bin = data_bin[0:1]
+
+        if operateur_bin == 1:
+            operateur = '+'
+        elif operateur_bin == 2:
+            operateur = '-'
+        elif operateur_bin == 3:
+            operateur = '*'
+        elif operateur_bin == 4:
+            operateur = '/'
+
+        numb1_bin = data_bin[9:28]
+        numb2_bin = data_bin[28:48]
+
+        print(int(numb1_bin, 2))
+        print(int(numb2_bin, 2))
         # Evaluation et envoi du résultat
         # res  = eval(data.decode())
         # conn.send(str(res).encode())
